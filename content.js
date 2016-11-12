a82d7a78ad9e73762a8fcb027a3bcb4b2e4d5997
Kana Mishra <kanam12@users.noreply.github.com>
2016-11-12 06:18:59 -0800

    Add files via upload

---
+++ content.js
@@ -0,0 +1,36 @@
+/**
+* Instant Thesaurus
+* Jeremy Sutton, 2013
+* Chrome Extension - Right-click thesaurus
+**/
+
+function clearMenu() {
+	chrome.extension.sendMessage({
+		request: 'clearContextMenu'
+	});
+}
+
+var current = null;
+document.addEventListener('mouseup', function(e) {
+	if (e.button == 0) {
+		var selection = window.getSelection().toString().trim();
+		// alert('hi');
+		// var selection = chrome.windows.getLastFocused(function(Window window) {
+		// window.getSelection().toString().trim();
+		// }
+		// );//.trim();
+		if ( selection.length > 2 && selection.length < 30 && ((selection.split(" ").length - 1) < 2) ) {
+			if (selection == current)
+				return;
+			current = selection;
+			chrome.extension.sendMessage({
+				request: 'updateContextMenu',
+				selection: selection
+			});
+		}
+		else {
+			clearMenu();
+			current = null;
+		}
+	}
+}, true);
---
+++ thesaurus.js
@@ -0,0 +1,149 @@
+/**
+* Instant Thesaurus
+* Jeremy Sutton, 2013
+* Chrome Extension - Right-click thesaurus
+**/
+
+// ID to manage the context menu entry
+var cmid;
+var submenus = [];
+
+function copyTextToClipboard(text) {
+  var copyFrom = document.createElement("textarea");
+  copyFrom.textContent = text;
+  var body = document.getElementsByTagName('body')[0];
+  body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand('copy');
+  body.removeChild(copyFrom);
+}
+
+
+function find_synonyms(word) {
+	var xhr = new XMLHttpRequest();
+	var params = "word=" + word + "&language=en_US&output=json&key=hYrjBiG559abNlfKVZzP";
+	var url = "http://thesaurus.altervista.org/thesaurus/v1?" + params;
+	var results;
+	var synonyms = [], antonyms = [];
+
+	xhr.open("GET", url, true);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+			var suboptions;
+			//update loading placeholder
+			suboptions = {
+				title: "Synonyms for " + word,
+				contexts: ['selection']
+			};
+			chrome.contextMenus.update(cmid, suboptions);
+				
+			try {
+				//window.alert(xhr.responseText);
+				results = JSON.parse(xhr.responseText);
+				results.response.forEach(function(list) {
+					list.list.synonyms.split("|").forEach(function(syn) {
+						synonyms.push(syn);
+					});
+				});
+			}
+			catch (err) {
+				//alert(err.constructor);
+				suboptions = {
+					title: "No synonyms found.",
+					contexts: ['selection']
+				};
+				chrome.contextMenus.update(cmid, suboptions);
+				return;
+			}
+			
+			if (synonyms.length == 0 && antonyms.length == 0){
+				suboptions = {
+					title: "No synonyms found.",
+					contexts: ['selection']
+				};
+				chrome.contextMenus.update(cmid, suboptions);
+				return;
+			}
+			
+			var smid; 
+			synonyms.forEach(function(syn) {
+				suboptions = {
+					title: syn,
+					parentId: cmid,
+					contexts: ['selection'],
+					onclick: function(info, tab) {
+						copyTextToClipboard(syn);
+					}
+				};
+				smid = chrome.contextMenus.create(suboptions);
+				
+			});
+			
+			if (antonyms.length > 0) {
+				suboptions = {
+					type: "separator",
+					title: "antonyms",
+					parentId: cmid,
+					contexts: ['selection']
+				};
+				smid = chrome.contextMenus.create(suboptions);
+				
+				antonyms.forEach(function(ant) {
+					suboptions = {
+						title: ant,
+						parentId: cmid,
+						contexts: ['selection'],
+						onclick: function(info, tab) {
+							copyTextToClipboard(ant);
+						}
+					};
+					smid = chrome.contextMenus.create(suboptions);
+					
+				});
+			}
+		}
+	};
+	
+	xhr.send();
+};
+
+
+var cm_clickHandler = function(clickData, tab) {
+	var synonyms = find_synonyms(clickData.selectionText);
+};
+
+chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+    if (msg.request === 'updateContextMenu') {
+		if (msg.selection == '') {
+			return false;
+		}
+		
+        var type = 'Synonyms for '+msg.selection;
+        if (type == '') {
+            // Remove the context menu entry
+            if (cmid != null) {
+                chrome.contextMenus.remove(cmid);
+                cmid = null; // Invalidate entry now to avoid race conditions
+            } // else: No contextmenu ID, so nothing to remove
+        } else { // Add/update context menu entry
+            chrome.contextMenus.removeAll();
+
+            var options = {
+                title: "Finding synonyms for " + msg.selection + "...(re-click)",
+                contexts: ['selection'],
+                onclick: cm_clickHandler
+            };
+
+            cmid = chrome.contextMenus.create(options, function(pid) {
+				var synonyms = find_synonyms(msg.selection);
+			});
+			
+        }
+    }
+	else if (msg.request == 'clearContextMenu') {
+		if (cmid != null) {
+            chrome.contextMenus.remove(cmid);
+			cmid = null;
+        }
+	}
+});
